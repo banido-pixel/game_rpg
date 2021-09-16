028b2a24ee6794a19796853bccdcc5ba106612c2
Grigory <wertyas1997@gmail.com>
2021-09-16 08:30:28 +0300

    initial coomit

---
+++ src/main/java/com/game/repository/PlayerRepository.java
@@ -0,0 +1,18 @@
+package com.game.repository;
+
+import com.game.entity.Player;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {
+
+    //Page<Player> findAll(Pageable pageable);
+}
